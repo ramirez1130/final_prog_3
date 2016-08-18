af118b393747f1dbb96217c68f21b6784ba2ff13
Brian <brian.ramirez@despegar.com>
2016-08-17 21:41:30 -0300

    Relacion arreglada del modelo tournament

--- app/Models/Tournament.php
+++ app/Models/Tournament.php
@@ -8,14 +8,14 @@ class Tournament extends Model{
 
 	protected $table = 'tournaments';
 
-	protected $fillable = ['name'];
+	protected $fillable = ['name','price','type_tournament_id'];
 
 	public function matches(){
 		$this->hasMany(Match::class);
 	}
 
 	public function typeTournament(){
-		return $this->hasOne(TypeTournament::class);
+		return $this->hasOne(TypeTournament::class,'id','type_tournament_id');
 	}
 
 }
