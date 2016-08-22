c36012b3a2b3b94ed404bd66bc86eeeb3d4cdc01
Brian <brian.ramirez@despegar.com>
2016-08-22 16:18:02 -0300

    Agregado laravel collective y debugbar. Libreria bootstrap para la vista

--- config/app.php
+++ config/app.php
@@ -158,6 +158,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         *  Providers new applications
+         */
+        Collective\Html\HtmlServiceProvider::class
     ],
 
     /*
@@ -203,6 +207,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        //Alias new applications
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class
 
     ],
 
